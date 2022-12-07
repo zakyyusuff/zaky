29953851c2bd7689c9cc8285719173a374e891e4
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2022-12-07 11:31:26 +0700

    Update fungsi2.py

--- fungsi2.py
+++ fungsi2.py
@@ -5,59 +5,34 @@ from nltk.stem.snowball import SnowballStemmer
 import sys
 
 def getNumberOfUppercaseWords(_list):
-    # "This calculate the number of words in uppercase in the message"
     n = 0
     for elem in _list:
         l = len(elem) #length of the token
-        u = sum(c.isupper() for c in elem) #number of character in uppercase       
+        u = sum(c.isupper() for c in elem)      
         if u==l: #if all characters are in uppercase
             n+=1
     return n
 
-# with open('data.arff', 'w') as out:
-#     out.write('@relation sms\n')
-#     out.write('\n@attribute ncharacter real\n')
-#     out.write('@attribute ndollars real\n')
-#     out.write('@attribute nnumeric real\n')
-#     out.write('@attribute nuppercase real\n')
-#     out.write('@attribute type {ham, spam}\n')
-#     out.write('\n@data\n')
 
 try:
     stemmer = SnowballStemmer("english")
     file = open('test_fungsi2.py', 'r')
-# ////////////// experiment
     sys.stdin= file
-    # execfile( "test_fungsi2.py" )
     exec( file )
-    # exec(open("test_fungsi2.py").read())
-# ///////////// experiment
 
     for line in file: 
         counter = Counter(line) #create a counter 
-        number_of_character = len(line) #number of character in the message
-        number_of_dollars = counter['$'] #number of dollars in the message            
+        number_of_character = len(line) 
+        number_of_dollars = counter['$']           
         number_of_numeric = sum(c.isdigit() for c in line) #number of numeric value in the message
-        line = line.translate(str.maketrans('.,;:/?!@#$&~#{([|-_^)]+=}*%', '                           ')) #remove special characters        
+        line = line.translate(str.maketrans('.,;:/?!@#$&~#{([|-_^)]+=}*%', '                           '))
         tokens = line.split() #split the message into words
-        number_of_uppercase_word = getNumberOfUppercaseWords(tokens) #number of words in uppercase in the message        
-        tokens = [stemmer.stem(token.lower()) for token in tokens] #convert all tokens into lowercase and find the word root
-        tokens.insert(0,number_of_character) #insert the number of character at 1st place
-        tokens.insert(1,number_of_dollars) #insert the number of dollars at 2nd place
-        tokens.insert(2,number_of_numeric) #insert the number of numerical value at 3rd place
-        tokens.insert(3,number_of_uppercase_word) #insert the number of words in uppercase at 4th place
-        
-        #print(tokens)
-        
-        # with open('data.arff', 'a') as out:
-        #     i = 0
-        #     for token in tokens[:5]:
-        #         i+=1
-        #         if i!=5:
-        #             out.write(str(token) + ',')
-        #         else:
-        #             out.write(str(token) + '\n')
-                
+        number_of_uppercase_word = getNumberOfUppercaseWords(tokens)
+        tokens = [stemmer.stem(token.lower()) for token in tokens]
+        tokens.insert(0,number_of_character)
+        tokens.insert(1,number_of_dollars)
+        tokens.insert(2,number_of_numeric)
+        tokens.insert(3,number_of_uppercase_word) 
         
         
 except Exception as e:
